4f6d5e8368f50f578bfb57420044509fa1cf4f33
CawaAlreadyTaken <daniele.cabassi01@gmail.com>
2023-10-13 00:09:54 +0200

    Aggiunta modalità Klein Bottle e Projective Plane

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,9 +24,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/apple-touch-icon.png": "4faf7c1194716f1008f0f5f6567404fa",
 "icons/favicon-16x16.png": "968c995aa13cfe8c60ffd5eef3c6a398",
 "icons/favicon-32x32.png": "a900dab878be9b30b67cba3f8dc0f5e8",
-"index.html": "7e2913306e7da617436f9702fed04192",
-"/": "7e2913306e7da617436f9702fed04192",
-"main.dart.js": "5874ef00f83d90ebd31d20cd5faf1946",
+"index.html": "d896d8ed51814393b0c7310b1e483f3d",
+"/": "d896d8ed51814393b0c7310b1e483f3d",
+"main.dart.js": "d5b3ac705e16a6469b2c8af6578754b2",
 "manifest.json": "304f155097db7c74f8b0ab4f08a7a617",
 "version.json": "caf28729a2613029e59af3648cda022f"};
 // The application shell files that are downloaded before a service worker can
